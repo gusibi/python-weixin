870e31e6e4de6a7fd75ddc239b16ed9d3c6d1ef5
Xindong Ding <dxd.spirits@gmail.com>
2016-11-08 02:32:56 +0800

    replace print statement by print() function in Sample.py

--- weixin/lib/Sample.py
+++ weixin/lib/Sample.py
@@ -20,7 +20,7 @@ if __name__ == "__main__":
    #测试加密接口
    encryp_test = WXBizMsgCrypt(token,encodingAESKey,appid)
    ret,encrypt_xml = encryp_test.EncryptMsg(to_xml,nonce)
-   print ret,encrypt_xml
+   print(ret,encrypt_xml)
 
 
    #测试解密接口
@@ -30,4 +30,4 @@ if __name__ == "__main__":
    from_xml = """<xml><ToUserName><![CDATA[gh_10f6c3c3ac5a]]></ToUserName><FromUserName><![CDATA[oyORnuP8q7ou2gfYjqLzSIWZf0rs]]></FromUserName><CreateTime>1409735668</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA[abcdteT]]></Content><MsgId>6054768590064713728</MsgId><Encrypt><![CDATA[hyzAe4OzmOMbd6TvGdIOO6uBmdJoD0Fk53REIHvxYtJlE2B655HuD0m8KUePWB3+LrPXo87wzQ1QLvbeUgmBM4x6F8PGHQHFVAFmOD2LdJF9FrXpbUAh0B5GIItb52sn896wVsMSHGuPE328HnRGBcrS7C41IzDWyWNlZkyyXwon8T332jisa+h6tEDYsVticbSnyU8dKOIbgU6ux5VTjg3yt+WGzjlpKn6NPhRjpA912xMezR4kw6KWwMrCVKSVCZciVGCgavjIQ6X8tCOp3yZbGpy0VxpAe+77TszTfRd5RJSVO/HTnifJpXgCSUdUue1v6h0EIBYYI1BD1DlD+C0CR8e6OewpusjZ4uBl9FyJvnhvQl+q5rv1ixrcpCumEPo5MJSgM9ehVsNPfUM669WuMyVWQLCzpu9GhglF2PE=]]></Encrypt></xml>"""
    decrypt_test = WXBizMsgCrypt(token,encodingAESKey,appid)
    ret ,decryp_xml = decrypt_test.DecryptMsg(from_xml, msg_sign, timestamp, nonce)
-   print ret ,decryp_xml
+   print(ret ,decryp_xml)
