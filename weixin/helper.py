3f59a13ca1ae0a29fe72995da341982b574478f5
goodspeed <cacique1103@gmail.com>
2017-03-14 18:36:43 +0800

    add custom msg send

--- weixin/helper.py
+++ weixin/helper.py
@@ -12,8 +12,11 @@ Description: Weixin helpers
 """
 
 import sys
-import types
+import datetime
 from hashlib import sha1
+from decimal import Decimal
+
+import six
 from six.moves import html_parser
 
 PY2 = sys.version_info[0] == 2
@@ -48,30 +51,155 @@ else:
         return x.decode(charset, errors)
 
 
-def smart_str(s, encoding='utf-8', strings_only=False, errors='strict'):
+class Promise(object):
+    """
+    This is just a base class for the proxy class created in
+    the closure of the lazy function. It can be used to recognize
+    promises in code.
+    """
+    pass
+
+
+class _UnicodeDecodeError(UnicodeDecodeError):
+    def __init__(self, obj, *args):
+        self.obj = obj
+        UnicodeDecodeError.__init__(self, *args)
+
+    def __str__(self):
+        original = UnicodeDecodeError.__str__(self)
+        return '%s. You passed in %r (%s)' % (original, self.obj,
+                                              type(self.obj))
+
+
+def smart_text(s, encoding='utf-8', strings_only=False, errors='strict'):
+    """
+    Returns a text object representing 's' -- unicode on Python 2 and str on
+    Python 3. Treats bytestrings using the 'encoding' codec.
+    If strings_only is True, don't convert (some) non-string-like objects.
+    """
+    if isinstance(s, Promise):
+        # The input is the result of a gettext_lazy() call.
+        return s
+    return force_text(s, encoding, strings_only, errors)
+
+
+_PROTECTED_TYPES = six.integer_types + (type(None), float, Decimal,
+                                        datetime.datetime, datetime.date,
+                                        datetime.time)
+
+
+def is_protected_type(obj):
+    """Determine if the object instance is of a protected type.
+    Objects of protected types are preserved as-is when passed to
+    force_text(strings_only=True).
+    """
+    return isinstance(obj, _PROTECTED_TYPES)
+
+
+def force_text(s, encoding='utf-8', strings_only=False, errors='strict'):
+    """
+    Similar to smart_text, except that lazy instances are resolved to
+    strings, rather than kept as lazy objects.
+    If strings_only is True, don't convert (some) non-string-like objects.
+    """
+    # Handle the common case first for performance reasons.
+    if issubclass(type(s), six.text_type):
+        return s
+    if strings_only and is_protected_type(s):
+        return s
+    try:
+        if not issubclass(type(s), six.string_types):
+            if six.PY3:
+                if isinstance(s, bytes):
+                    s = six.text_type(s, encoding, errors)
+                else:
+                    s = six.text_type(s)
+            elif hasattr(s, '__unicode__'):
+                s = six.text_type(s)
+            else:
+                s = six.text_type(bytes(s), encoding, errors)
+        else:
+            # Note: We use .decode() here, instead of six.text_type(s, encoding,
+            # errors), so that if s is a SafeBytes, it ends up being a
+            # SafeText at the end.
+            s = s.decode(encoding, errors)
+    except UnicodeDecodeError as e:
+        if not isinstance(s, Exception):
+            raise _UnicodeDecodeError(s, *e.args)
+        else:
+            # If we get to here, the caller has passed in an Exception
+            # subclass populated with non-ASCII bytestring data without a
+            # working unicode method. Try to handle this without raising a
+            # further exception by individually forcing the exception args
+            # to unicode.
+            s = ' '.join(force_text(arg, encoding, strings_only, errors)
+                         for arg in s)
+    return s
+
+
+def smart_bytes(s, encoding='utf-8', strings_only=False, errors='strict'):
     """
     Returns a bytestring version of 's', encoded as specified in 'encoding'.
     If strings_only is True, don't convert (some) non-string-like objects.
     """
-    if strings_only and isinstance(s, (types.NoneType, int)):
+    if isinstance(s, Promise):
+        # The input is the result of a gettext_lazy() call.
         return s
-    if not isinstance(s, basestring):
+    return force_bytes(s, encoding, strings_only, errors)
+
+
+def force_bytes(s, encoding='utf-8', strings_only=False, errors='strict'):
+    """
+    Similar to smart_bytes, except that lazy instances are resolved to
+    strings, rather than kept as lazy objects.
+    If strings_only is True, don't convert (some) non-string-like objects.
+    """
+    # Handle the common case first for performance reasons.
+    if isinstance(s, bytes):
+        if encoding == 'utf-8':
+            return s
+        else:
+            return s.decode('utf-8', errors).encode(encoding, errors)
+    if strings_only and is_protected_type(s):
+        return s
+    if isinstance(s, Promise):
+        return six.text_type(s).encode(encoding, errors)
+    if not isinstance(s, six.string_types):
         try:
-            return str(s)
+            if six.PY3:
+                return six.text_type(s).encode(encoding)
+            else:
+                return bytes(s)
         except UnicodeEncodeError:
             if isinstance(s, Exception):
                 # An Exception subclass containing non-ASCII data that doesn't
                 # know how to print itself properly. We shouldn't raise a
                 # further exception.
-                return ' '.join([smart_str(arg, encoding, strings_only,
-                                           errors) for arg in s])
-            return unicode(s).encode(encoding, errors)
-    elif isinstance(s, unicode):
-        return s.encode(encoding, errors)
-    elif s and encoding != 'utf-8':
-        return s.decode('utf-8', errors).encode(encoding, errors)
+                return b' '.join(force_bytes(arg, encoding,
+                                             strings_only, errors)
+                                 for arg in s)
+            return six.text_type(s).encode(encoding, errors)
     else:
-        return s
+        return s.encode(encoding, errors)
+
+if six.PY3:
+    smart_str = smart_text
+    force_str = force_text
+else:
+    smart_str = smart_bytes
+    force_str = force_bytes
+    # backwards compatibility for Python 2
+    smart_unicode = smart_text
+    force_unicode = force_text
+
+smart_str.__doc__ = """
+Apply smart_text in Python 3 and smart_bytes in Python 2.
+This is suitable for writing to sys.stdout (for instance).
+"""
+
+force_str.__doc__ = """
+Apply force_text in Python 3 and force_bytes in Python 2.
+"""
 
 
 def genarate_js_signature(params):
--- weixin/reply.py
+++ weixin/reply.py
@@ -5,15 +5,25 @@ from __future__ import unicode_literals
 
 import time
 
+from .helper import smart_str
 from .msg_template import (TEXT_TEMPLATE, IMAGE_TEMPLATE,
                            VOICE_TEMPLATE, VIDEO_TEMPLATE,
                            THUM_MUSIC_TEMPLATE, NOTHUM_MUSIC_TEMPLATE,
                            ARITICLE_TEMPLATE, ARITICLE_ITEM_TEMPLATE)
 
 
+class Article(object):
+    def __init__(self, title=None, description=None, picurl=None, url=None):
+        self.title = title or ''
+        self.description = description or ''
+        self.picurl = picurl or ''
+        self.url = url or ''
+
+
 class WXReply(object):
 
-    def __init__(self, to_user=None, from_user=None, create_time=None, **kwargs):
+    def __init__(self, to_user=None, from_user=None,
+                 create_time=None, **kwargs):
         '''
         MsgType: text|image|voice|video|music|news
         '''
@@ -130,6 +140,13 @@ class ArticleReply(WXReply):
         super(ArticleReply, self).__init__(**kwargs)
         self._articles = []
 
+    def add_article(self, article):
+        if len(self._articles) >= 8:
+            raise AttributeError(
+                "Can't add more than 8 articles in an ArticleReply")
+        else:
+            self._articles.append(article)
+
     def render(self):
         items = []
         for article in self._articles:
@@ -142,3 +159,157 @@ class ArticleReply(WXReply):
         self.params["items"] = ''.join(items)
         self.params["count"] = len(items)
         return self.TEMPLATE.format(**self.params)
+
+
+class WXCustomReply(object):
+
+    def __init__(self, to_user=None, msgtype=None, **kwargs):
+        '''
+        MsgType: text|image|voice|video|music|news
+        '''
+        kwargs['to_user'] = to_user
+        kwargs['msgtype'] = msgtype
+
+        self.params = {k: v for k, v in kwargs.items() if kwargs[k]}
+
+    def render(self):
+        raise NotImplementedError()
+
+
+class CustomTextReply(WXCustomReply):
+    """
+    回复文字消息
+    """
+
+    def __init__(self, content, *args, **kwargs):
+        """
+        :param content: 文字回复内容
+        """
+        super(CustomTextReply, self).__init__(msgtype='text',
+                                              *args, **kwargs)
+        self.content = smart_str(content)
+
+    def render(self):
+        self.params['text'] = {
+            'content': self.content
+        }
+        return self.params
+
+
+class CustomImageReply(WXCustomReply):
+    """
+    回复图片消息
+    """
+
+    def __init__(self, media_id):
+        """
+        :param media_id: 图片的 MediaID
+        """
+        super(CustomImageReply, self).__init__(msgtype='image')
+        self.media_id = media_id
+
+    def render(self):
+        self.params['image'] = {
+            'media_id': self.media_id
+        }
+        return self.params
+
+
+class CustomVoiceReply(WXCustomReply):
+    """
+    回复语音消息
+    """
+
+    def __init__(self, media_id):
+        """
+        :param media_id: 语音的 MediaID
+        """
+        super(CustomVoiceReply, self).__init__(msgtype='voice')
+        self.media_id = media_id
+
+    def render(self):
+        self.params['image'] = {
+            'media_id': self.media_id
+        }
+        return self.params
+
+
+class CustomVideoReply(WXCustomReply):
+    """
+    回复视频消息
+    """
+
+    def __init__(self, media_id, title=None, description=None):
+        """
+        :param media_id: 视频的 MediaID
+        :param title: 视频消息的标题
+        :param description: 视频消息的描述
+        """
+        super(CustomVideoReply, self).__init__(msgtype='music')
+        self.media_id = media_id
+        self.title = title or ''
+        self.description = description or ''
+
+    def render(self):
+        self.params['video'] = {
+            'media_id': self.media_id,
+            'title': self.title,
+            'description': self.description
+        }
+        return self.params
+
+
+class CustomMusicReply(WXCustomReply):
+    """
+    回复音乐消息
+    """
+
+    def __init__(self, title='', description='', music_url='',
+                 hq_music_url='', thumb_media_id=None):
+        self.title = title or ''
+        self.description = description or ''
+        self.musicurl = music_url or ''
+        self.hqmusicurl = hq_music_url or music_url
+        self.thumb_media_id = thumb_media_id
+        super(CustomMusicReply, self).__init__(msgtype='music')
+
+    def render(self):
+        self.params['music'] = {
+            'title': self.title,
+            'description': self.description,
+            'musicurl': self.musicurl,
+            'hqmusicurl': self.hqmusicurl,
+            'thumb_media_id': self.thumb_media_id,
+        }
+        return self.params
+
+
+class CustomArticleReply(WXCustomReply):
+
+    def __init__(self, **kwargs):
+        self._articles = []
+        super(CustomArticleReply, self).__init__(msgtype='news', **kwargs)
+
+    def add_article(self, article):
+        '''
+        :params article (dict)
+        {
+            "title":"Happy Day",
+            "description":"Is Really A Happy Day",
+            "url":"URL",
+            "picurl":"PIC_URL"
+        }
+
+        '''
+        if len(self._articles) >= 8:
+            raise AttributeError(
+                "Can't add more than 8 articles in an CustomArticleReply")
+        else:
+            self._articles.append(article)
+
+    def render(self):
+        items = []
+        for article in self._articles:
+            items.append(article)
+        self.params['news'] = {'articles': items}
+        return self.params
