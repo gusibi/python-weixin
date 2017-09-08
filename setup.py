169c1f13ce38c8193a796d158c16c747275854c1
goodspeed <cacique1103@gmail.com>
2017-09-08 15:35:36 +0800

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 setup(name="python-weixin",
-      version="0.2.1",
+      version="0.2.2",
       description="Python Weixin API client support wechat-app",
       license="BSD",
       install_requires=["simplejson", "requests", "six", "chardet"],
